26c0ad4f583a88bdf23d409b332f9099b0b947c0
Yongliang Shi <yongliang.michael.shi@gmail.com>
2020-09-24 11:29:36 -0500

    add acquire.py and prepare.py

---
+++ acquire.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+import pandas as pd
+import os
+import env
+
+
+# ### 1. Make a function named `get_titanic_data` that returns the titanic data from the codeup data science database as a pandas data frame. Obtain your data from the Codeup Data Science Database.
+
+# In[ ]:
+
+
+def get_connection(db, user=env.user, host=env.host, password=env.password):
+    return f'mysql+pymysql://{user}:{password}@{host}/{db}'
+
+
+# In[ ]:
+
+
+def get_titanic_data():
+    filename = 'titanic.csv'
+    
+    if os.path.isfile(filename):
+        return pd.read_csv(filename, index_col=0)
+    else:
+        df = pd.read_sql('select * from passengers', get_connection('titanic_db'))
+        df.to_csv(filename)
+        return df
+
+
+# ### 2. Make a function named `get_iris_data` that returns the data from the `iris_db` on the codeup data science database as a pandas data frame. The returned data frame should include the actual name of the species in addition to the `species_ids`. Obtain your data from the Codeup Data Science Database.
+
+# In[9]:
+
+
+def get_iris_data():
+    filename = 'iris.csv'
+    
+    if os.path.isfile(filename):
+        return pd.read_csv(filename, index_col=0)
+    else: 
+        df = pd.read_sql("""select * from measurements join species using(species_id)""", get_connection('iris_db'))
+        df.to_csv(filename)
+        return df
+
+
+# In[ ]:
+
+
+
+
---
+++ prepare.py
@@ -0,0 +1,62 @@
+import numpy as np
+import pandas as pd
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+from sklearn.model_selection import train_test_split
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder, MinMaxScaler
+
+from pydataset import data
+
+import warnings
+warnings.filterwarnings("ignore")
+
+def cleaning(titanic):
+    drop_index = titanic[titanic.embarked.isnull()].index
+    titanic.drop(index=drop_index, inplace=True)
+    titanic.drop(columns=['deck', 'passenger_id'], inplace=True)
+    dummies = pd.get_dummies(titanic[['sex','embarked']], drop_first=True)
+    titanic = pd.concat([titanic, dummies], axis=1)
+    titanic.drop(columns=['sex', 'embarked','class','embark_town'], inplace=True)
+    return titanic
+
+def cleaning_spliting(titanic):
+    titanic = cleaning(titanic)
+    train_validate, test = train_test_split(titanic, test_size=0.2, 
+                                            random_state=123,
+                                            stratify=titanic.survived
+                                           )
+    train, validate = train_test_split(train_validate, test_size=0.3, 
+                                       random_state=123, 
+                                       stratify=train_validate.survived
+                                      )
+    return train, validate, test
+
+def prep_titanic(titanic):
+    train, validate, test = cleaning_spliting(titanic)
+    imputer = SimpleImputer(strategy = 'most_frequent')
+    imputer = imputer.fit(train[['age']])
+    train[['age']] = imputer.transform(train[['age']])
+    validate[['age']] = imputer.transform(validate[['age']])
+    test[['age']] = imputer.transform(test[['age']])
+    return train, validate, test
+
+def prep_titanic_mean(titanic):
+    train, validate, test = cleaning_spliting(titanic)
+    imputer = SimpleImputer(strategy = 'mean')
+    imputer = imputer.fit(train[['age']])
+    train[['age']] = imputer.transform(train[['age']])
+    validate[['age']] = imputer.transform(validate[['age']])
+    test[['age']] = imputer.transform(test[['age']])
+    return train, validate, test
+
+def prep_iris(iris):
+    iris.drop(columns=['species_id','measurement_id'], inplace=True)
+    iris.rename(columns={'species_name':'species'}, inplace=True)
+    species_dummy = pd.get_dummies(iris[['species']])
+    iris = pd.concat([iris, species_dummy], axis=1)
+    train_validate, test = train_test_split(iris, test_size=.2, random_state=123, stratify=iris.species)
+    train, validate = train_test_split(train_validate, test_size=.3, random_state=123, stratify=train_validate.species)
+    return train, validate, test
