95fb66504e6afb96b18596d69e398104dc751265
Yongliang Shi <yongliang.michael.shi@gmail.com>
2020-10-01 10:56:40 -0500

    create function compare_slr_metrics to produce a table of metrics of simple linear regression on multiple continuous features

--- evaluate.py
+++ evaluate.py
@@ -8,11 +8,15 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 from statsmodels.formula.api import ols
+from sklearn.metrics import mean_squared_error
+from math import sqrt
+
 # %%
 def evaluate_slr(target, feature, df):
     """
     Returns evaluation table for simple linear regression model
     Parameters: target var(str), feature(str), data
+    Baseline: mean
     """
     baseline = df[target].mean()
     formula = target + ' ~ ' + feature
@@ -28,7 +32,7 @@ def evaluate_slr(target, feature, df):
 def plot_residuals(actual, predicted):
     """
     Returns the scatterplot of actural y in horizontal axis and residuals in vertical axis
-    Parameters: actural y, predicted y
+    Parameters: actural y(df.se), predicted y(df.se)
     """
     residuals = actual - predicted
     plt.hlines(0, actual.min(), actual.max(), ls=':')
@@ -37,3 +41,39 @@ def plot_residuals(actual, predicted):
     plt.xlabel('actual value ($y$)')
     plt.title('Actual vs Residual')
     return plt.gca()
+# %%
+def slr_metrics(target, feature, df):
+    """
+    Returns a list of SSE, MSE, RMSE, $R^2$, and f_pval
+    Parameters: target var(str), feature(str), data source(df)
+    Baseline: mean
+    """
+    baseline = df[target].mean()
+    formula = target + ' ~ ' + feature
+    model = ols(formula, df).fit()
+    x = df[feature]
+    y_actural = df[target]
+    y_pred = model.predict()
+    baseline_residual = y_actural - baseline
+    baseline_sse = (baseline_residual**2).sum()
+    model_residual = y_actural - y_pred
+    model_sse = (model_residual**2).sum()
+    model_mse = mean_squared_error(y_actural, y_pred)
+    model_rmse = sqrt(model_mse)
+    r2 = model.rsquared
+    f_pval = model.f_pvalue
+    metrics = [baseline_sse, model_sse, model_mse, model_rmse, r2, f_pval]
+    return metrics
+# %%
+def compare_slr_metrics(target, features, df):
+    """
+    Compare the metrics of simple linear regression on multiple continuous features
+    Parameters: target var(str), interested features(list), data source(df)
+    Prerequisite: import function `slr_metrics`
+    Baseline: mean
+    """
+    metrics = pd.DataFrame(index=['baseline_sse', 'model_sse', 'model_mse', 'model_rmse', 'r2', 'f_pval'])
+    for feature in features:
+        col = slr_metrics(target, feature, df)
+        metrics[feature] = col
+    return metrics.T
