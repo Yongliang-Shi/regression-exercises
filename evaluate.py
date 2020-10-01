10b6e3d2766f2168c20ff60ef64d86a9a9b91b31
Yongliang Shi <yongliang.michael.shi@gmail.com>
2020-10-01 09:43:07 -0500

    add function plot_residuals to create the scatterplot of residuals against actural y

--- evaluate.py
+++ evaluate.py
@@ -4,13 +4,16 @@ warnings.filterwarnings("ignore")
 import numpy as np
 import pandas as pd
 
+import matplotlib.pyplot as plt
+import seaborn as sns
+
 from statsmodels.formula.api import ols
 # %%
 def evaluate_slr(target, feature, df):
-    '''
+    """
     Returns evaluation table for simple linear regression model
     Parameters: target var(str), feature(str), data
-    '''
+    """
     baseline = df[target].mean()
     formula = target + ' ~ ' + feature
     model = ols(formula, df).fit()
@@ -20,3 +23,17 @@ def evaluate_slr(target, feature, df):
     evaluate['baseline_residual'] = evaluate[target] - evaluate.baseline
     evaluate['model_residual'] = evaluate[target] - evaluate[(target + '_pred')]
     return evaluate
+
+# %%
+def plot_residuals(actual, predicted):
+    """
+    Returns the scatterplot of actural y in horizontal axis and residuals in vertical axis
+    Parameters: actural y, predicted y
+    """
+    residuals = actual - predicted
+    plt.hlines(0, actual.min(), actual.max(), ls=':')
+    plt.scatter(actual, residuals)
+    plt.ylabel('residual ($y - \hat{y}$)')
+    plt.xlabel('actual value ($y$)')
+    plt.title('Actual vs Residual')
+    return plt.gca()
