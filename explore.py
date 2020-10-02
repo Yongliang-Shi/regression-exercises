3b399924cdf8a7f2a8fbce5201f3feb7722c5509
Yongliang Shi <yongliang.michael.shi@gmail.com>
2020-10-02 14:03:25 -0500

    add functions select_kbest and rfe for feature engineering

--- explore.py
+++ explore.py
@@ -1,5 +1,16 @@
+import warnings
+warnings.filterwarnings("ignore")
+
+import numpy as np
+import pandas as pd
+
 import matplotlib.pyplot as plt
 import seaborn as sns
+
+from sklearn.feature_selection import SelectKBest, f_regression
+from sklearn.linear_model import LinearRegression
+from sklearn.feature_selection import RFE
+
 #%%
 def plot_variable_pairs(df):
     return sns.pairplot(df, kind='reg')
@@ -17,3 +28,28 @@ def plot_categorical_and_continuous_vars(df, categorical, continuous):
 def corr_heatmap(df):
     corr = df.corr()
     sns.heatmap(corr, cmap='Blues', annot=True)
+
+# %%
+def select_kbest(X, y, n):
+    """
+    Returns the top n selected features based on the SelectKBest calss
+    Parameters: predictors(X) in df, target(y) in df, the number of features to select(n)
+    """
+    f_selector = SelectKBest(f_regression, k=n)
+    f_selector = f_selector.fit(X, y)
+    f_support = f_selector.get_support()
+    f_feature = X.iloc[:, f_support].columns.tolist()
+    return f_feature
+
+# %%
+def rfe(X, y, n):
+    """
+    Returns the top n selected features based on the RFE calss
+    Parameters: predictors(X) in df, target(y) in df, the number of features to select(n)
+    """
+    lm = LinearRegression()
+    rfe = RFE(lm, n)
+    rfe = rfe.fit(X, y)
+    rfe_support = rfe.support_
+    f_feature = X.iloc[:, rfe_support].columns.tolist()
+    return f_feature
